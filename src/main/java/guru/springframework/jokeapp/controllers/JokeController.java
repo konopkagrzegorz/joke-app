232bae6dba5620ee67ced13faffc0c580a37ef0e
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-06-08 11:40:47 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/jokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package guru.springframework.jokeapp.controllers;
+
+import guru.springframework.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @GetMapping({"/",""})
+    public String showRandomJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
